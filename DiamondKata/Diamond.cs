f16434537e3a039f3bafd3998f401217577f6ca8
mavi <mavisulie@gmail.com>
2022-06-20 12:32:33 +0100

    Added comments to the Diamond class

--- DiamondKata/Diamond.cs
+++ DiamondKata/Diamond.cs
@@ -2,24 +2,36 @@
 
 namespace DiamondKata
 {
+    /// <summary>
+    /// Rhombus representation of a given letter of the alphabet as midpoint
+    /// </summary>
     public class Diamond
     {
+        /// <summary>
+        /// Initialises <see cref="Diamond"
+        /// </summary>
         public Diamond(char pointer) => Build(pointer);
 
+        // The rhombus as string
         public StringBuilder _printableOutput;
 
+        /// <summary>
+        /// Displays the rhombus string
+        /// </summary>
         public void Print() => Console.WriteLine(_printableOutput);
 
         private void Build(int pointer)
         {
-            pointer -= 65;
+            pointer -= 65; // Start from A char as 0 index
             _printableOutput = new StringBuilder();
 
+            // Build first half
             for (int i = 0; i <= pointer; i++)
             {
                 AppendRowString(i, Convert.ToChar('A' + i), pointer);
             }
 
+            // Build second half
             for (int i = pointer - 1; i >= 0; i--)
             {
                 AppendRowString(i, Convert.ToChar('A' + i), pointer);
@@ -34,7 +46,7 @@ namespace DiamondKata
             _printableOutput.Append(string.Concat(Enumerable.Repeat(" ", sideSpaces)));
             _printableOutput.Append(value);
             _printableOutput.Append(string.Concat(Enumerable.Repeat(" ", innerSpaces)));
-            if (value != 'A')
+            if (value != 'A') // Only print one character if A
                 _printableOutput.Append(value);
             _printableOutput.Append(string.Concat(Enumerable.Repeat(" ", sideSpaces)));
             _printableOutput.Append("\n");
